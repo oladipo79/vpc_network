6d4b6208b26e742a80621595ba6e9700090822ef
Oladipo Ogundipe <oladipo79@gmail.com>
2023-09-20 10:23:42 +0100

    added natgw route to private rt

--- private_rt.tf
+++ private_rt.tf
@@ -1,6 +1,12 @@
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
 
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id
+  }
+
+
   tags = {
     Name = "private_rt"
   }
